844120c03ed2f184ff6b45dec02f48e5da7d5bbf
Youbin Park <a44121078@gmail.com>
2023-04-13 14:44:25 +0900

    'week01_complete'

--- week01/Variable_Sorting.py
+++ week01/Variable_Sorting.py
@@ -143,7 +143,7 @@ if __name__ == '__main__':
     # bubble_sort(a)
     # insertion_sort(a)
     # quick_sort(a, 0, len(a) - 1)
-    # merge_sort(a, 0, len(a) - 1)
+    merge_sort(a, 0, len(a) - 1)
     # heap_sort(a)
     # counting_sort(a)
     print(f'정렬 후: {a}')
